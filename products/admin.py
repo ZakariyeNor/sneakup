5b139b26c8ec0947d86e8c3981d9187fe1ed155f
Zakariye Farah Nor <0e6xrww7@students.codeinstitute.net>
2025-06-30 13:08:24 +0000

    Customize products and categories admin

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,47 @@
 from django.contrib import admin
+from .models import Category, Product
 
-# Register your models here.
+@admin.register(Product)
+# Product admin
+class ProductAdmin(admin.ModelAdmin):
+    """
+    Customize how the data properties look like on the admin page
+    """
+    list_display = (
+        'sku',
+        'name',
+        'category',
+        'price',
+        'rating',
+        'image',
+    )
+
+    list_filter = (
+        'category',
+    )
+
+    search_fields = (
+        'category',
+    )
+    ordering = (
+        'sku',
+        'name',
+        'category',
+    )
+
+
+@admin.register(Category)
+# Caregory admin 
+class CaregoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+    list_filter = (
+        'friendly_name',
+    )
+
+    search_fields = (
+        'name',
+    )
