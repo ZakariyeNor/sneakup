e79bbc2aab1c51d4ef4095dba6276b82b71bbc69
Zakariye Farah Nor <0e6xrww7@students.codeinstitute.net>
2025-07-04 08:59:03 +0000

    Added event handlers for the increment and decrement buttons of the qty update form

--- static/js/base.js
+++ static/js/base.js
@@ -41,7 +41,7 @@ document.addEventListener('DOMContentLoaded', function () {
     const decrementBtn = document.getElementById('decrement-btn');
 
     incrementBtn.addEventListener('click', function(e) {
-        e.preventDefault
+        e.preventDefault();
         const max = parseInt(quantity.max);
         let current = parseInt(quantity.value);
         if (current < max) quantity.value = current + 1;
@@ -57,4 +57,40 @@ document.addEventListener('DOMContentLoaded', function () {
     console.log('Selected size:', this.dataset.size);
     console.log('quantity:', current);
 
+    /* Increment the specific item in the shopping bag, to attach click listeners to all increment buttons */
+    document.querySelectorAll('button[id^="increment-qty-btn"]').forEach(button => {
+        button.addEventListener('click', function (event) {
+            event.preventDefault();
+            console.log('Clicked:', event.target.id);
+            /* Get the id from the event or the product */
+            const productId = event.target.id.split('-').pop();
+            console.log('Product ID:', productId);
+            const qtyInput = document.getElementById(`qty-input-${productId}`);
+            console.log('Quantity Input:', qtyInput);
+            /* Get the maximum value from the input value */
+            const max = parseInt(qtyInput.max);
+            const currentQty = parseInt(qtyInput.value);
+            if (currentQty < max) {
+                qtyInput.value = currentQty + 1;
+            }
+
+        });
+    });
+
+    /* Decrement the specific item in the shopping bag, to attach click listeners to all decrement buttons */
+    document.querySelectorAll('button[id^="decrement-qty-btn"]').forEach(button => {
+        button.addEventListener('click', function (event) {
+            event.preventDefault();
+            console.log('Clicked:', event.target.id);
+            const productId = event.target.id.split('-').pop();
+            console.log('Product ID:', productId);
+            const qtyInput = document.getElementById(`qty-input-${productId}`);
+            console.log('Quantity Input:', qtyInput);
+            const min = parseInt(qtyInput.min);
+            const currentQty = parseInt(qtyInput.value);
+            if (currentQty > min) {
+                qtyInput.value = currentQty - 1;
+            }
+        });
+    });
 });
