c1df3a7fae3cb73682854d8e753dbd81bd925e82
Zakariye Farah Nor <0e6xrww7@students.codeinstitute.net>
2025-07-08 06:46:26 +0000

    Register Order and OrderLineItem models in admin with custom fields, read-only settings, search, and ordering by date

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,3 +1,74 @@
 from django.contrib import admin
+from .models import Order, OrderLineItem
 
-# Register your models here.
+
+
+# Inline admin for order line
+class OrderLineItemAdminInline(admin.TabularInline):
+    """
+    Inline admin configuration for displaying order line items 
+    within the Order admin detail view.
+    """
+    model = OrderLineItem
+    readonly_fields = ('lineitem_total',)
+    
+
+# Register Order with the admin
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    inlines = (OrderLineItemAdminInline,)
+    readonly_fields = (
+        'order_number', 'first_name',
+        'last_name', 'date', 'delivery',
+        'order_total', 'grand_total',
+    )
+
+    list_display = (
+        'order_number',
+        'first_name',
+        'last_name',
+        'email',
+        'phone_number',
+        'city',
+        'street_address_1',
+        'street_address_2',
+        'postcode',
+        'county',
+        'country',
+        'order_total',
+        'delivery',
+        'vat',
+        'grand_total',
+        'date',
+    )
+
+    list_filter = ('date', 'country', 'city')
+    search_fields = (
+        'order_number',
+        'first_name',
+        'last_name',
+        'email',
+        'phone_number',
+        'postcode',
+        'city',
+        'street_address_1',
+    )
+    fields = (
+        'order_number',
+        'first_name',
+        'last_name',
+        'date',
+        'email',
+        'phone_number',
+        'country',
+        'postcode',
+        'city',
+        'street_address_1',
+        'street_address_2',
+        'county',
+        'delivery',
+        'order_total',
+        'vat',
+        'grand_total',
+    )
+    ordering = ('-date',)
