67b2174ea7efb9c801027373d1f473c657386abc
Zakariye Farah Nor <0e6xrww7@students.codeinstitute.net>
2025-07-10 06:01:49 +0000

    Added realtime validation on stripe card. Each field displays the error if it's one otherwise it'll be hiiden span

--- checkout/static/js/checkout.js
+++ checkout/static/js/checkout.js
@@ -30,3 +30,45 @@ var cardCvc = elements.create("cardCvc", { style: style });
 cardNumber.mount("#card-number-element");
 cardExpiry.mount("#card-expiry-element");
 cardCvc.mount("#card-cvc-element");
+
+/* Validation errors on the card number, date and cvc
+ and showing them by field */
+
+function showErrorByField(event, elementId) {
+  const errorSpan = document.getElementById(elementId);
+  if (event.error) {
+    errorSpan.innerHTML = `
+      <span class="text-danger" role="alert">
+        <i class="fas fa-times me-1"></i>
+        ${event.error.message}
+      </span>
+    `;
+    errorSpan.classList.remove('d-none')
+  } else {
+    errorSpan.textContent = '';
+    errorSpan.classList.add('d-none')
+  }
+}
+
+// Then attach event listeners to each Stripe Element
+cardNumber.on('change', (event) => showErrorByField(event, 'card-number-error-display'));
+cardExpiry.on('change', (event) => showErrorByField(event, 'card-expiry-error-display'));
+cardCvc.on('change', (event) => showErrorByField(event, 'card-cvc-error-display'));
+
+/* Display global card errors unrelated
+ to specific fields */
+function showGlobalCardError(message) {
+  const globalError = document.getElementById('card-errors-display');
+  if (message) {
+    globalError.innerHTML = `
+      <span class="text-danger" role="alert">
+        <i class="fas fa-times me-1"></i>
+        ${message}
+      </span>
+    `;
+    globalError.classList.remove('d-none');
+  } else {
+    globalError.textContent = '';
+    globalError.classList.add('d-none');
+  }
+}
