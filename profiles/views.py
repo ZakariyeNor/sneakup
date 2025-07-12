60f339b0f098a1fb59839f79055c57e1bc417857
Zakariye Farah Nor <0e6xrww7@students.codeinstitute.net>
2025-07-12 17:15:55 +0000

    Create personla info, delivery info and order history layout in to the profile template

--- profiles/views.py
+++ profiles/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Profile
 
 # Profile view
@@ -6,8 +6,11 @@ def profile(request):
     """
     User profile view
     """
+    profile = get_object_or_404(Profile, user=request.user)
     return render(
         request,
         template_name= 'profiles/profile.html',
-        context = {}
+        context = {
+            'profile': profile,
+        }
     )
