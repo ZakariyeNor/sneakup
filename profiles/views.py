1d99b2dc453a12eb8c5d8b893274500759b6b4d5
Zakariye Farah Nor <0e6xrww7@students.codeinstitute.net>
2025-07-12 14:59:09 +0000

    Create view, url and profile template

--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .models import Profile
 
-# Create your views here.
+# Profile view
+def profile(request):
+    """
+    User profile view
+    """
+    return render(
+        request,
+        template_name= 'profiles/profile.html',
+        context = {}
+    )
