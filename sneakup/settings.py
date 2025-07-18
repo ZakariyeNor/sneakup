e11d8d2cd306c70169d25472d0c5ca4f1cec50b4
Zakariye Farah Nor <0e6xrww7@students.codeinstitute.net>
2025-07-18 17:23:58 +0000

    Update images

--- sneakup/settings.py
+++ sneakup/settings.py
@@ -208,11 +208,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / "static"]
-""" STATIC_ROOT = BASE_DIR / "staticfiles"
-STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage' """
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 
 
 MEDIA_URL = '/media/'
