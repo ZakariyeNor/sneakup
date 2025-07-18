f305a9c9c1c5a973ae1799007f84533b44245fae
Zakariye Farah Nor <0e6xrww7@students.codeinstitute.net>
2025-07-18 13:49:05 +0000

    Update url for bag

--- bag/urls.py
+++ bag/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('bag/', views.view_bag, name='bag'),
+    path('', views.view_bag, name='bag'),
     path('add/<int:item_id>/', views.add_to_bag, name='add_to_bag'),
     path('bag/update/<item_id>/', views.update_bag, name='update_bag'),
 ]
